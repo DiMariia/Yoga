c1c43c58f4fa0be23ddb9fe9eeab9c0f9dd0f566
Demoniolik <mdidenko27@gmail.com>
2022-07-15 21:53:22 +0300

    add change to styles

--- dev/js/script.js
+++ dev/js/script.js
@@ -19,6 +19,7 @@ $(window).scroll(function(){
 
   });
 
+
 const adaptiveResize = (header) => {
   var actualHeight = header.css("width");
   var actualHeightNumber = retrieveNumberFromStringWithPx(actualHeight);
@@ -36,3 +37,38 @@ const adaptiveResize = (header) => {
 const retrieveNumberFromStringWithPx = (stringWithPx) => {
   return parseInt(stringWithPx.replace("px", ""));
 }
+
+// scroll when you click on the menu
+
+$(document).ready(function(){
+  $("#menu").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
+
+$(document).ready(function() {
+  $('.menu-item').click(function(event) {
+      $('.header__burger, .header__menu').removeClass('active');
+
+  })
+})
+
+// rating
+for (var i=1; i<5; i++) {
+      (function(N){
+          var container = document.querySelector('.rating_block'+N);
+          var items = container.querySelectorAll('.label_rating')
+          container.onclick = function(e) {
+              if( ! e.target.classList.contains('active') ){
+                  items.forEach(function(item){
+                      item.classList.remove('active');
+                  });
+                  e.target.classList.add('active');
+              }
+          }
+      })(i);
+  };
+  
